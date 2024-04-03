b9dfbe550a67f35da63512206c79ee25e3fb02e4
Rohitvarfa <rohitvarfa731@gmail.com>
2024-04-03 14:58:28 +0530

    session and cookies

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,47 @@
 var express = require('express');
 var router = express.Router();
-const userModel = require("./users");
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get("/", function(req, res) {
+//   req.session.ban = true;
+//   res.render("index");
+// });
+
+
+router.get("/checkban", function(req, res) {
+  if(req.session.ban===true){
+  res.send("You are banned");
+}
+else{
+  res.send("not banned");
+}
 });
 
-//create
-router.get('/create',async function(req, res, next) {
-  const createdUser = await userModel.create({
-        username:"rohit",
-        age:20,
-        name:"Rohit"
-       });
- res.send(createdUser);
+//remove ban
+router.get("/removeban", function(req, res) {
+  req.session.destroy(function(err){
+    if (err) throw err;
+    res.send("ban removed");
+  });
 });
 
-//read
-router.get('/allusers',async function(req, res, next) {
- const allusers = await userModel.find();
- res.send(allusers);
+//cookie
+router.get("/",function(req,res){
+  res.cookie("age",25);
+  res.render("index");
+});
+router.get("/read",function(req,res){
+  // req.cookies("age",25);
+  console.log(req.cookies.age);
+  res.send("check");
 });
 
-// read one
-router.get('/allusers1',async function(req, res, next) {
- const allusers = await userModel.findOne({username:"Rohit"});
- console.log(allusers);
- res.send(allusers);
-});
-
-
-
-
-//delete
-router.get('/delete', async function(req, res, next) {
-const deleteduser = await userModel.findOneAndDelete({
-    username : "rohit"
-  })
-  console.log(deleteduser);
-  res.send(deleteduser);
+router.get("/delete",function(req,res){
+  // req.cookies("age",25);
+  console.log(req.cookies.age);
+  res.clearCookie("age");
+  res.send("clear hogyi");
 });
 
 module.exports = router;
